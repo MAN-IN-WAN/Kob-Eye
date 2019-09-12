05d1c525ca5eb7791f33a1e49ce14a5ac54adafd
polgo <paul@abtel.fr>
2019-09-12 12:17:28 +0200

    event logtext

--- Skins/LoginCadref/Modules/Default/Default.twig.php
+++ Skins/LoginCadref/Modules/Default/Default.twig.php
@@ -2,4 +2,3 @@
 $vars['logform'] = isset($_POST['logform']) ? $_POST['logform'] : '';
 $classe = isset($_GET['classe']) ? $_GET['classe'] : '';
 if(!empty($classe)) $vars['classe'] = "/?classe=$classe";
-klog::l("x>>>>>>>>>>>>>>>>>>>>>>>$classe<<<<<<<<<");
