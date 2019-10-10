92832d1ff6eb3bd45d6cbf83ff2db3b92441f82a
enguerr <enguer@enguer.com>
2019-10-10 14:28:47 +0200

    Delete clearcache.sh

--- clearcache.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-rm Home/*/.cache -Rf
-rm Home/*/.sessions -Rf
-rm Modules/*/.Db.cache -f
-rm Skins/*/.cache -Rf
-find . -name "*.mini.*.jpg" -delete
-find . -name "*.limit.*.jpg" -delete
