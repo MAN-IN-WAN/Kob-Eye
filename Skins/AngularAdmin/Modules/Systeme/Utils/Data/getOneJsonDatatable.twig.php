23e1513cee5557551153ff33ed2e3f9644b685ea
polgo <paul@abtel.fr>
2019-11-05 17:57:07 +0100

    chrono

--- Skins/AngularAdmin/Modules/Systeme/Utils/Data/getOneJsonDatatable.twig.php
+++ Skins/AngularAdmin/Modules/Systeme/Utils/Data/getOneJsonDatatable.twig.php
@@ -17,6 +17,7 @@ if (!$vars['row']){
 }
 Event::registerPush($info['Module'],$info['ObjectType'],$info['ObjectType'],'~',0,1,$context,$vars['row']->Id);
 
+if(Sys::$remote_addr == '10.0.3.185') die('+++++++++++++++++++++'.Sys::$remote_addr);
 
 
 $vars['row']->label = $vars['row']->getFirstSearchOrder();
@@ -28,7 +29,6 @@ else $vars['row']->userCreateName = 'inconnu';
 if (is_object($ue))
     $vars['row']->userEditName = $ue->Login;
 else $vars['row']->userEditName = 'inconnu';
-if(Sys::$remote_addr == '10.0.3.185') die('+++++++++++++++++++++'.Sys::$remote_addr);
 
 foreach ($vars['fields'] as $f){
     if ($f['type']=='date'){
