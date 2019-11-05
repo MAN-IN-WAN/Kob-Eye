fcce6c7cd5186c74a3877dadf519911a90589206
polgo <paul@abtel.fr>
2019-11-05 18:00:10 +0100

    chrono

--- Skins/AngularAdmin/Modules/Systeme/Utils/Data/getOneJsonDatatable.twig.php
+++ Skins/AngularAdmin/Modules/Systeme/Utils/Data/getOneJsonDatatable.twig.php
@@ -19,16 +19,15 @@ Event::registerPush($info['Module'],$info['ObjectType'],$info['ObjectType'],'~',
 
 
 
-$vars['row']->label = $vars['row']->getFirstSearchOrder();
-$uc = Sys::getOneData('Systeme','User/'.$vars['row']->userCreate);
-$ue = Sys::getOneData('Systeme','User/'.$vars['row']->userEdit);
-if (is_object($uc))
-    $vars['row']->userCreateName = $uc->Login;
-else $vars['row']->userCreateName = 'inconnu';
-if (is_object($ue))
-    $vars['row']->userEditName = $ue->Login;
-else $vars['row']->userEditName = 'inconnu';
-if(Sys::$remote_addr == '10.0.3.185') die('+++++++++++++++++++++'.Sys::$remote_addr);
+//$vars['row']->label = $vars['row']->getFirstSearchOrder();
+//$uc = Sys::getOneData('Systeme','User/'.$vars['row']->userCreate);
+//$ue = Sys::getOneData('Systeme','User/'.$vars['row']->userEdit);
+//if (is_object($uc))
+//    $vars['row']->userCreateName = $uc->Login;
+//else $vars['row']->userCreateName = 'inconnu';
+//if (is_object($ue))
+//    $vars['row']->userEditName = $ue->Login;
+//else $vars['row']->userEditName = 'inconnu';
 
 foreach ($vars['fields'] as $f){
     if ($f['type']=='date'){
