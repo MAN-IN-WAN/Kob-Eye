0d992bcacad2d44517a0268325914792f0dd369d
Mogwaili <guillaume.candella@gmail.com>
2020-05-27 22:25:06 +0200

    minisite

--- Modules/Parc/Class/PageMiniSite.class.php
+++ Modules/Parc/Class/PageMiniSite.class.php
@@ -2,4 +2,22 @@
 
 class PageMiniSite extends genericClass{
 
+    public function getParams($full=false){
+        $params = $this->getChildren('ParametreMiniSite');
+        if($full){
+            $modele = $this->getOneParent('ModeleMiniSite');
+            $pms = $modele->getChildren('ParametreMiniSite');
+            $params = $params + $pms;
+        }
+        return $params;
+    }
+
+    public function getParamsValues($msID,$full=false){
+        $params = $this->getParams($full);
+        foreach ($params as &$p){
+            $val = $p->getOneChild('ValeurMiniSite/MiniSite.MiniSiteId('.$msID.')');
+            $p->vms = $val->Valeur;
+        }
+        return $params;
+    }
 }
